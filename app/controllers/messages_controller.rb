1974ce396234c6bd565df8351381d49e74b58ecb
Hoang Huy Ngoc <hoanghuyngoc@gmail.com>
2016-07-27 12:56:03 +0000

    Deploy to heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,10 +10,17 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
+    @message.ip = request.remote_ip
+    
     if @message.save
       redirect_to root_path
     else
       render 'new'
     end
+  end
+  
+  private 
+  def message_params
+    params.require(:message).permit(:content, :ip)
   end
 end
